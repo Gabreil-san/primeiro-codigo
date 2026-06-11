66e3b64fdf485cf40cdb34028dd3fbfdef0d77f6
Angel Gabriel <angel_pino@estudante.sesisenai.org.br>
2026-06-10 21:53:44 -0300

    par e impar

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ Console.Write("Informe um numero: ");
 // Cria a variável variavelQueSalvaUmNumero,
 // Leia o que o usuario digitar
 // e transforma o valor lido de texto para inteiro (int.parse)
-int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine());
+int variavelQueSalvaUmNumero = int.Parse(Console.ReadLine()!);
 
 // verifica se o valor de variavelQueSalvaUmNumero é par
 if (variavelQueSalvaUmNumero % 2 == 0) {
